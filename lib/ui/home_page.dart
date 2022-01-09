94e20f966bae6e4a4aef5b3647f4fde58e4c4b6a
pirate-kor <pirate2003@gmail.com>
2022-01-09 21:41:17 +0900

    notification 코드 추가
    아이폰, 안드로이드 설정 notification 설정 어려움..
    
    참고. https://www.dbestech.com/tutorials/flutter-local-notification-explained-for-ios-and-android

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todoapp/service/notification_service.dart';
 import 'package:todoapp/service/theme_service.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  var notifyHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    notifyHelper.requestIOSPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
       // backgroundColor: Colors.white,
-      body: Column(children: [
-        Text("Theme Datas",
-        style: TextStyle(fontSize: 30),)
-      ],),
-      
+      body: Column(
+        children: [
+          Text(
+            "Theme Datas",
+            style: TextStyle(fontSize: 30),
+          )
+        ],
+      ),
     );
   }
 
@@ -27,12 +42,25 @@ class _HomePageState extends State<HomePage> {
       leading: GestureDetector(
         onTap: () {
           ThemeService().switchTheme();
+          notifyHelper.displayNotification(
+              title: "theme changed",
+              body: Get.isDarkMode
+                  ? "Activated Light Theme"
+                  : "Activated Dark Theme");
+
+          notifyHelper.scheduledNotification();
         },
-        child: Icon(Icons.nightlight_round, size: 20,),
+        child: Icon(
+          Icons.nightlight_round,
+          size: 20,
+        ),
       ),
       actions: [
-        Icon(Icons.person, size: 20,),
-        SizedBox(width: 20),  
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(width: 20),
       ],
     );
   }
